d0b425d7344291102f628592a9d3e7345958df6b
devbok0209 <devbok0209@bodycodi.com>
2022-06-06 18:10:39 +0900

    FigureCreator 중복 제거

--- src/main/java/coordinate/FigureCreator.java
+++ src/main/java/coordinate/FigureCreator.java
@@ -5,22 +5,13 @@ import java.util.List;
 public interface FigureCreator {
 
     static Figure line(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
-        }
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        return new Line(points);
     }
 
     static Figure triangle(List<Point> points) {
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        return new Triangle(points);
     }
     static Figure rectangle(List<Point> points) {
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        return new Rectangle(points);
     }
 }
